041c19f83e562e66e8fde0bae0803e2dea9ce363
LiCHO <lichothu@gmail.com>
2025-06-28 20:30:05 -0400

    fix comfig

--- scripts/train_cond_mnist.py
+++ scripts/train_cond_mnist.py
@@ -62,7 +62,7 @@ def parse_args() -> argparse.Namespace:
     p.add_argument('--wandb_run_name', type=str, default='flow_matching')
     p.add_argument('--no_wandb', action='store_true')
     # IB hyperparameters
-    p.add_argument('--use_ib', type="bool", default=False, help='Enable Information Bottleneck regularization')
+    p.add_argument('--use_ib', type=bool, default=False, help='Enable Information Bottleneck regularization')
     p.add_argument('--ib_lambda', type=float, default=1e-1, help='Weight for kinetic-energy penalty')
     p.add_argument('--ib_beta', type=float, default=1e-4, help='Weight for entropy regularizer')
     return p.parse_args()
